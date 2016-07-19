266f13d73812f35555ce6e096bec45be663a1796
justcoding121 <jehonathant@gmail.com>
2016-07-18 23:52:13 -0400

    Add NTLM/Kerberos auth helpers

---
+++ Titanium.Web.Proxy/Authentication/Interop/SecurityContext.cs
@@ -0,0 +1,270 @@
+﻿/// <summary>
+/// http://www.pinvoke.net/default.aspx/secur32.initializesecuritycontext
+/// </summary>
+namespace Titanium.Web.Proxy.Authentication.Security
+{
+
+    using System;
+    using System.Runtime.InteropServices;
+
+    public enum SecBufferType
+    {
+        SECBUFFER_VERSION = 0,
+        SECBUFFER_EMPTY = 0,
+        SECBUFFER_DATA = 1,
+        SECBUFFER_TOKEN = 2
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct SecHandle //=PCtxtHandle
+    {
+        IntPtr dwLower; // ULONG_PTR translates to IntPtr not to uint
+        IntPtr dwUpper; // this is crucial for 64-Bit Platforms
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct SecBuffer : IDisposable
+    {
+        public int cbBuffer;
+        public int BufferType;
+        public IntPtr pvBuffer;
+
+
+        public SecBuffer(int bufferSize)
+        {
+            cbBuffer = bufferSize;
+            BufferType = (int)SecBufferType.SECBUFFER_TOKEN;
+            pvBuffer = Marshal.AllocHGlobal(bufferSize);
+        }
+
+        public SecBuffer(byte[] secBufferBytes)
+        {
+            cbBuffer = secBufferBytes.Length;
+            BufferType = (int)SecBufferType.SECBUFFER_TOKEN;
+            pvBuffer = Marshal.AllocHGlobal(cbBuffer);
+            Marshal.Copy(secBufferBytes, 0, pvBuffer, cbBuffer);
+        }
+
+        public SecBuffer(byte[] secBufferBytes, SecBufferType bufferType)
+        {
+            cbBuffer = secBufferBytes.Length;
+            BufferType = (int)bufferType;
+            pvBuffer = Marshal.AllocHGlobal(cbBuffer);
+            Marshal.Copy(secBufferBytes, 0, pvBuffer, cbBuffer);
+        }
+
+        public void Dispose()
+        {
+            if (pvBuffer != IntPtr.Zero)
+            {
+                Marshal.FreeHGlobal(pvBuffer);
+                pvBuffer = IntPtr.Zero;
+            }
+        }
+    }
+
+    public struct MultipleSecBufferHelper
+    {
+        public byte[] Buffer;
+        public SecBufferType BufferType;
+
+        public MultipleSecBufferHelper(byte[] buffer, SecBufferType bufferType)
+        {
+            if (buffer == null || buffer.Length == 0)
+            {
+                throw new ArgumentException("buffer cannot be null or 0 length");
+            }
+
+            Buffer = buffer;
+            BufferType = bufferType;
+        }
+    };
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct SecBufferDesc : IDisposable
+    {
+
+        public int ulVersion;
+        public int cBuffers;
+        public IntPtr pBuffers; //Point to SecBuffer
+
+        public SecBufferDesc(int bufferSize)
+        {
+            ulVersion = (int)SecBufferType.SECBUFFER_VERSION;
+            cBuffers = 1;
+            SecBuffer ThisSecBuffer = new SecBuffer(bufferSize);
+            pBuffers = Marshal.AllocHGlobal(Marshal.SizeOf(ThisSecBuffer));
+            Marshal.StructureToPtr(ThisSecBuffer, pBuffers, false);
+        }
+
+        public SecBufferDesc(byte[] secBufferBytes)
+        {
+            ulVersion = (int)SecBufferType.SECBUFFER_VERSION;
+            cBuffers = 1;
+            SecBuffer ThisSecBuffer = new SecBuffer(secBufferBytes);
+            pBuffers = Marshal.AllocHGlobal(Marshal.SizeOf(ThisSecBuffer));
+            Marshal.StructureToPtr(ThisSecBuffer, pBuffers, false);
+        }
+
+        public SecBufferDesc(MultipleSecBufferHelper[] secBufferBytesArray)
+        {
+            if (secBufferBytesArray == null || secBufferBytesArray.Length == 0)
+            {
+                throw new ArgumentException("secBufferBytesArray cannot be null or 0 length");
+            }
+
+            ulVersion = (int)SecBufferType.SECBUFFER_VERSION;
+            cBuffers = secBufferBytesArray.Length;
+
+            //Allocate memory for SecBuffer Array....
+            pBuffers = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(SecBuffer)) * cBuffers);
+
+            for (int Index = 0; Index < secBufferBytesArray.Length; Index++)
+            {
+                //Super hack: Now allocate memory for the individual SecBuffers
+                //and just copy the bit values to the SecBuffer array!!!
+                SecBuffer ThisSecBuffer = new SecBuffer(secBufferBytesArray[Index].Buffer, secBufferBytesArray[Index].BufferType);
+
+                //We will write out bits in the following order:
+                //int cbBuffer;
+                //int BufferType;
+                //pvBuffer;
+                //Note that we won't be releasing the memory allocated by ThisSecBuffer until we
+                //are disposed...
+                int CurrentOffset = Index * Marshal.SizeOf(typeof(SecBuffer));
+                Marshal.WriteInt32(pBuffers, CurrentOffset, ThisSecBuffer.cbBuffer);
+                Marshal.WriteInt32(pBuffers, CurrentOffset + Marshal.SizeOf(ThisSecBuffer.cbBuffer), ThisSecBuffer.BufferType);
+                Marshal.WriteIntPtr(pBuffers, CurrentOffset + Marshal.SizeOf(ThisSecBuffer.cbBuffer) + Marshal.SizeOf(ThisSecBuffer.BufferType), ThisSecBuffer.pvBuffer);
+            }
+        }
+
+        public void Dispose()
+        {
+            if (pBuffers != IntPtr.Zero)
+            {
+                if (cBuffers == 1)
+                {
+                    SecBuffer ThisSecBuffer = (SecBuffer)Marshal.PtrToStructure(pBuffers, typeof(SecBuffer));
+                    ThisSecBuffer.Dispose();
+                }
+                else
+                {
+                    for (int Index = 0; Index < cBuffers; Index++)
+                    {
+                        //The bits were written out the following order:
+                        //int cbBuffer;
+                        //int BufferType;
+                        //pvBuffer;
+                        //What we need to do here is to grab a hold of the pvBuffer allocate by the individual
+                        //SecBuffer and release it...
+                        int CurrentOffset = Index * Marshal.SizeOf(typeof(SecBuffer));
+                        IntPtr SecBufferpvBuffer = Marshal.ReadIntPtr(pBuffers, CurrentOffset + Marshal.SizeOf(typeof(int)) + Marshal.SizeOf(typeof(int)));
+                        Marshal.FreeHGlobal(SecBufferpvBuffer);
+                    }
+                }
+
+                Marshal.FreeHGlobal(pBuffers);
+                pBuffers = IntPtr.Zero;
+            }
+        }
+
+        public byte[] GetSecBufferByteArray()
+        {
+            byte[] Buffer = null;
+
+            if (pBuffers == IntPtr.Zero)
+            {
+                throw new InvalidOperationException("Object has already been disposed!!!");
+            }
+
+            if (cBuffers == 1)
+            {
+                SecBuffer ThisSecBuffer = (SecBuffer)Marshal.PtrToStructure(pBuffers, typeof(SecBuffer));
+
+                if (ThisSecBuffer.cbBuffer > 0)
+                {
+                    Buffer = new byte[ThisSecBuffer.cbBuffer];
+                    Marshal.Copy(ThisSecBuffer.pvBuffer, Buffer, 0, ThisSecBuffer.cbBuffer);
+                }
+            }
+            else
+            {
+                int BytesToAllocate = 0;
+
+                for (int Index = 0; Index < cBuffers; Index++)
+                {
+                    //The bits were written out the following order:
+                    //int cbBuffer;
+                    //int BufferType;
+                    //pvBuffer;
+                    //What we need to do here calculate the total number of bytes we need to copy...
+                    int CurrentOffset = Index * Marshal.SizeOf(typeof(SecBuffer));
+                    BytesToAllocate += Marshal.ReadInt32(pBuffers, CurrentOffset);
+                }
+
+                Buffer = new byte[BytesToAllocate];
+
+                for (int Index = 0, BufferIndex = 0; Index < cBuffers; Index++)
+                {
+                    //The bits were written out the following order:
+                    //int cbBuffer;
+                    //int BufferType;
+                    //pvBuffer;
+                    //Now iterate over the individual buffers and put them together into a
+                    //byte array...
+                    int CurrentOffset = Index * Marshal.SizeOf(typeof(SecBuffer));
+                    int BytesToCopy = Marshal.ReadInt32(pBuffers, CurrentOffset);
+                    IntPtr SecBufferpvBuffer = Marshal.ReadIntPtr(pBuffers, CurrentOffset + Marshal.SizeOf(typeof(int)) + Marshal.SizeOf(typeof(int)));
+                    Marshal.Copy(SecBufferpvBuffer, Buffer, BufferIndex, BytesToCopy);
+                    BufferIndex += BytesToCopy;
+                }
+            }
+
+            return (Buffer);
+        }
+
+        /*public SecBuffer GetSecBuffer()
+        {
+            if(pBuffers == IntPtr.Zero)
+            {
+                throw new InvalidOperationException("Object has already been disposed!!!");
+            }
+
+            return((SecBuffer)Marshal.PtrToStructure(pBuffers,typeof(SecBuffer)));
+        }*/
+    }
+
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct SECURITY_INTEGER
+    {
+        public uint LowPart;
+        public int HighPart;
+        public SECURITY_INTEGER(int dummy)
+        {
+            LowPart = 0;
+            HighPart = 0;
+        }
+    };
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct SECURITY_HANDLE
+    {
+        public IntPtr LowPart;
+        public IntPtr HighPart;
+        public SECURITY_HANDLE(int dummy)
+        {
+            LowPart = HighPart = IntPtr.Zero;
+        }
+    };
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct SecPkgContext_Sizes
+    {
+        public uint cbMaxToken;
+        public uint cbMaxSignature;
+        public uint cbBlockSize;
+        public uint cbSecurityTrailer;
+    };
+
+}
---
+++ Titanium.Web.Proxy/Authentication/WindowsAuthHelper.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+
+namespace Titanium.Web.Proxy.Authentication.Security
+{
+    /// <summary>
+    /// Adapated from
+    /// http://www.pinvoke.net/default.aspx/secur32.initializesecuritycontext
+    /// </summary>
+    public class WindowsAuthHelper
+    {
+        public const int SEC_E_OK = 0;
+        public const int SEC_I_CONTINUE_NEEDED = 0x90312;
+        const int SECPKG_CRED_OUTBOUND = 2;
+        const int SECURITY_NATIVE_DREP = 0x10;
+        const int MAX_TOKEN_SIZE = 12288;
+
+        SECURITY_HANDLE _hOutboundCred = new SECURITY_HANDLE(0);
+        public SECURITY_HANDLE _hClientContext = new SECURITY_HANDLE(0);
+
+        public const int ISC_REQ_REPLAY_DETECT = 0x00000004;
+        public const int ISC_REQ_SEQUENCE_DETECT = 0x00000008;
+        public const int ISC_REQ_CONFIDENTIALITY = 0x00000010;
+        public const int ISC_REQ_CONNECTION = 0x00000800;
+
+        public const int STANDARD_CONTEXT_ATTRIBUTES = ISC_REQ_CONFIDENTIALITY | ISC_REQ_REPLAY_DETECT | ISC_REQ_SEQUENCE_DETECT | ISC_REQ_CONNECTION;
+
+        /// <summary>
+        /// computes the client response for server Authentication challenge tokens
+        /// </summary>
+        /// <param name="serverToken"></param>
+        /// <param name="bContinueProcessing">Indicates if susequent calls to server is expected to finish authentication</param>
+        /// <returns>client response token to server as bytes</returns>
+        public byte[] ComputeClientResponse(byte[] serverToken, out bool bContinueProcessing)
+        {
+            byte[] clientToken;
+
+            string _sAccountName = WindowsIdentity.GetCurrent().Name;
+            SECURITY_INTEGER ClientLifeTime = new SECURITY_INTEGER(0);
+
+            if (AcquireCredentialsHandle(_sAccountName, "NTLM", SECPKG_CRED_OUTBOUND,
+                                            IntPtr.Zero, IntPtr.Zero, 0, IntPtr.Zero,
+                                            ref _hOutboundCred, ref ClientLifeTime) != SEC_E_OK)
+            {
+                throw new Exception("Couldn't acquire client credentials");
+            }
+
+
+            int ss = -1;
+
+            SecBufferDesc ClientToken = new SecBufferDesc(MAX_TOKEN_SIZE);
+
+            try
+            {
+                uint ContextAttributes = 0;
+
+                if (serverToken == null)
+                {
+                    ss = InitializeSecurityContext(
+                        ref _hOutboundCred,
+                        IntPtr.Zero,
+                        _sAccountName,                  // null string pszTargetName,
+                        STANDARD_CONTEXT_ATTRIBUTES,
+                        0,                              //int Reserved1,
+                        SECURITY_NATIVE_DREP,           //int TargetDataRep
+                        IntPtr.Zero,                    //Always zero first time around...
+                        0,                              //int Reserved2,
+                        out _hClientContext,            //pHandle CtxtHandle = SecHandle
+                        out ClientToken,                //ref SecBufferDesc pOutput, //PSecBufferDesc
+                        out ContextAttributes,          //ref int pfContextAttr,
+                        out ClientLifeTime);            //ref IntPtr ptsExpiry ); //PTimeStamp
+
+                }
+                else
+                {
+                    SecBufferDesc ServerToken = new SecBufferDesc(serverToken);
+
+                    try
+                    {
+                        ss = InitializeSecurityContext(
+                            ref _hOutboundCred,
+                            ref _hClientContext,
+                            _sAccountName,                                  // null string pszTargetName,
+                            STANDARD_CONTEXT_ATTRIBUTES,
+                            0,                                              //int Reserved1,
+                            SECURITY_NATIVE_DREP,                           //int TargetDataRep
+                            ref ServerToken,                                //Always zero first time around...
+                            0,                                              //int Reserved2,
+                            out _hClientContext,                            //pHandle CtxtHandle = SecHandle
+                            out ClientToken,                                //ref SecBufferDesc pOutput, //PSecBufferDesc
+                            out ContextAttributes,                          //ref int pfContextAttr,
+                            out ClientLifeTime);                            //ref IntPtr ptsExpiry ); //PTimeStamp
+                    }
+                    finally
+                    {
+                        ServerToken.Dispose();
+                    }
+                }
+
+                if (ss != SEC_E_OK && ss != SEC_I_CONTINUE_NEEDED)
+                {
+                    throw new Exception("InitializeSecurityContext() failed!!!");
+                }
+
+                clientToken = ClientToken.GetSecBufferByteArray();
+            }
+            finally
+            {
+                ClientToken.Dispose();
+            }
+
+            bContinueProcessing = ss != SEC_E_OK;
+
+            return clientToken;
+        }
+
+        public string ComputeClientNTLMChallengeRespose(string challengeFromServer)
+        {
+            return null;
+        }
+
+        #region Native calls to secur32.dll
+
+        [DllImport("secur32", CharSet = CharSet.Auto)]
+        static extern int AcquireCredentialsHandle(
+                            string pszPrincipal,                //SEC_CHAR*
+                            string pszPackage,                  //SEC_CHAR* //"Kerberos","NTLM","Negotiative"
+                            int fCredentialUse,
+                            IntPtr PAuthenticationID,           //_LUID AuthenticationID,//pvLogonID, //PLUID
+                            IntPtr pAuthData,                   //PVOID
+                            int pGetKeyFn,                      //SEC_GET_KEY_FN
+                            IntPtr pvGetKeyArgument,            //PVOID
+                            ref SECURITY_HANDLE phCredential,   //SecHandle //PCtxtHandle ref
+                            ref SECURITY_INTEGER ptsExpiry);    //PTimeStamp //TimeStamp ref
+
+        [DllImport("secur32", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern int InitializeSecurityContext(ref SECURITY_HANDLE phCredential,//PCredHandle
+                            IntPtr phContext,                   //PCtxtHandle
+                            string pszTargetName,
+                            int fContextReq,
+                            int Reserved1,
+                            int TargetDataRep,
+                            IntPtr pInput,                      //PSecBufferDesc SecBufferDesc
+                            int Reserved2,
+                            out SECURITY_HANDLE phNewContext,   //PCtxtHandle
+                            out SecBufferDesc pOutput,          //PSecBufferDesc SecBufferDesc
+                            out uint pfContextAttr,             //managed ulong == 64 bits!!!
+                            out SECURITY_INTEGER ptsExpiry);    //PTimeStamp
+
+        [DllImport("secur32", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern int InitializeSecurityContext(ref SECURITY_HANDLE phCredential,   //PCredHandle
+                            ref SECURITY_HANDLE phContext,                              //PCtxtHandle
+                            string pszTargetName,
+                            int fContextReq,
+                            int Reserved1,
+                            int TargetDataRep,
+                            ref SecBufferDesc SecBufferDesc,                            //PSecBufferDesc SecBufferDesc
+                            int Reserved2,
+                            out SECURITY_HANDLE phNewContext,                           //PCtxtHandle
+                            out SecBufferDesc pOutput,                                  //PSecBufferDesc SecBufferDesc
+                            out uint pfContextAttr,                                     //managed ulong == 64 bits!!!
+                            out SECURITY_INTEGER ptsExpiry);                            //PTimeStamp
+
+        #endregion
+    }
+}
